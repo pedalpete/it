235eea778ab595fe0809b2d0514ff11e11b658df
Pete Field <pedalpete@hotmail.com>
2014-03-25 10:17:52 +1100

    first level of getting components

---
+++ tests/device_abilities.spec.js
@@ -0,0 +1,27 @@
+var $$ = require('../lib/device_abilities.js');
+
+describe("device build", function(){
+   it("should describe the structure of the device", function(){
+      var build =$$();
+      expect(build.name).toBe('Test-Device');   
+   });
+});
+
+describe("components", function(){
+    it("should find a matching single component", function(){
+        var led = $$('led');
+        expect(led.address).toBe(1);
+    });
+    
+    it("should match multiple components", function(){
+        var leds = $$('leds');
+        expect(leds[0].address).toBe(1);
+        expect(leds[1].address).toBe(2);
+    });
+    
+    it("should match equivelant components eg: led = lights, tri-color-led, etc.", function(){
+        var leds = $$('leds');
+        expect(leds[2].address).toBe(3);
+        expect(leds[3].address[0]).toBe(4);
+    });
+});
