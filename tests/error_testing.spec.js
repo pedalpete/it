811cfb6a14c6f2347140799e2ef13724deb004d2
Pete Field <pedalpete@hotmail.com>
2014-04-04 21:56:26 +1100

    added error handling as base

---
+++ tests/error_testing.spec.js
@@ -0,0 +1,10 @@
+var $$ = require('../lib/favor_obj_builder.js')('./tests/mock_favorit.json');
+
+describe('error handling', function(){
+    it('should allow the user to change the error handling', function(){
+        var led = $$('led');
+        spyOn(led, "onError");
+        led.set('new');
+        expect(led.onError).toHaveBeenCalled();
+    });
+});
