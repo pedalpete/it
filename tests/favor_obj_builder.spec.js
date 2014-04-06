c4ce2d72e14450d0f0c23904fe111fed87237c51
Pete Field <pedalpete@hotmail.com>
2014-04-06 13:49:39 +1000

    changed file setup to sync so the entire library initializes before returning the module

--- tests/favor_obj_builder.spec.js
+++ tests/favor_obj_builder.spec.js
@@ -1,8 +1,8 @@
 var fs = require('fs-extra');
 var gpioPath = 'tests/gpio-test/class/gpio';
 //setup the filesystem with the exports path
-fs.outputFileSync(gpioPath+'/export',0);
-for(var i=0;i<6;i++){
+fs.outputFileSync(gpioPath+'/export','none');
+for(var i=0;i<=6;i++){
     fs.outputFileSync(gpioPath+i+'/value',2,'utf-8');
     fs.outputFileSync(gpioPath+i+'/direction','none','utf-8');
 }
@@ -20,17 +20,14 @@ describe("device build", function(){
     
         expect($$().getGpioPath()).toBe(gpioPath);
         
-            /* for some reason, these tests aren't working. 
-            Will test on device and see if I get a better result.
-
+           
             expect(parseInt(fs.readFileSync(gpioPath+'/export','utf-8'),10)).toBeGreaterThan(0);
             
             var pin1d = fs.readFileSync(gpioPath+'1/direction','utf-8');
             var pin1v = fs.readFileSync(gpioPath+'1/value','utf-8');
-            console.log(pin1d, pin1v);
             expect(pin1d).toBe('out');
             expect(parseInt(pin1v,10)).toBe(0);
-            */
+           
       
     });
     
--- tests/getters_setters_spec.js
+++ tests/getters_setters_spec.js
@@ -1,13 +1,14 @@
 var $$ =  require('../lib/favor_obj_builder.js')('./tests/mock_favorit.json');
 
 describe("setters getters", function(){
-    var led = $$('led');
-    expect(led.get()).toBe(0);
-    led.set(1);
-    expect(led.get()).toBe(1);
-    led.set('low');
-    expect(led.get()).toBe(0);
-    led.set('high');
-    expect(led.get()).toBe(1);
-  
+    it("should get and set the values for the led", function(){
+        var led = $$('led');
+        expect(led.get()).toBe(0);
+        led.set(1);
+        expect(led.get()).toBe(1);
+        led.set('low');
+        expect(led.get()).toBe(0);
+        led.set('high');
+        expect(led.get()).toBe(1);
+    });
 });
