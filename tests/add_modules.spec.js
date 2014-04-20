bc200c66500a984cb4a18bfa234e0349c5b24930
Pete Field <pedalpete@hotmail.com>
2014-04-21 09:41:57 +1000

    returning the query and parsed_query as part of the favor_obj

--- tests/add_modules.spec.js
+++ tests/add_modules.spec.js
@@ -2,7 +2,6 @@ var $$ =  require('../index.js')('./tests/mock_favorit.json');
 
 describe('setting the device object prototype methods', function(){
     it('should add the get and set methods to the prototype of the global device object',function(){
-        
         expect($$().get).toBeDefined();   
         expect($$('led').get).toBeDefined();
     });
