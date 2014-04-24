f64d34fc9a8bdedfcf074e02ec5671ffe6ae086c
Pete Field <pedalpete@hotmail.com>
2014-04-24 05:26:24 +0000

    updated gpio example

--- examples/gpio.js
+++ examples/gpio.js
@@ -7,17 +7,21 @@ $$('button').onChange(function(val){
      led_get = $$('led').get()[0];
 	console.log(led_get);
      switch(led_get){
-        case {blue:0,green:0}:
+        case (led_get.blue===0 && led_get.green===0):
+		console.log('turn blue on');
 		return $$('led').set(1,'blue');
         	break;
-	case {blue:1,green:0}:
+	case (led_get.blue===1 && led_get.green===0):
+		console.log('turn blue off, turn green on');
  		$$('led').set(0,'blue');
 		return $$('led').set(1,'green');
 		break;
-	case {blue:0,green:1}:
+	case (led_get.blue===0 && led_get.green===1):
+		console.log('turn on both blue and green');
 		return $$('led').set(1,'blue');
 		break;
 	default:
+		console.log('turn off all');
 		$$('led').set(0);
      }
      
