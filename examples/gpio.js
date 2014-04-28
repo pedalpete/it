ed0510ba6b2d42b2f9b6fb0e7d5320d8c85b4fa7
Pete Field <pedalpete@hotmail.com>
2014-04-28 06:18:24 +0000

    added setting classes specifically on set

--- examples/gpio.js
+++ examples/gpio.js
@@ -3,26 +3,15 @@ var $$ = require('../index.js')();
 var led = $$('led').set(0);
 var led_get = $$('led').get();
 // attach an event to the button click
-$$('button').onChange(function(val){
-     led_get = $$('led').get()[0];
-	console.log(led_get);
-     switch(led_get){
-        case (led_get.blue===0 && led_get.green===0):
-		console.log('turn blue on');
-		return $$('led').set(1,'blue');
-        	break;
-	case (led_get.blue===1 && led_get.green===0):
-		console.log('turn blue off, turn green on');
- 		$$('led').set(0,'blue');
-		return $$('led').set(1,'green');
-		break;
-	case (led_get.blue===0 && led_get.green===1):
-		console.log('turn on both blue and green');
-		return $$('led').set(1,'blue');
-		break;
-	default:
-		console.log('turn off all');
-		$$('led').set(0);
-     }
-     
-});
+$$('button').onChange(changeLights);
+
+function changeLights(){
+led_get = $$('led').get()[0];
+	if(led_get.blue===0 && led_get.green===0)return led.set(1,'blue');
+	if(led_get.blue===1 && led_get.green===0){
+		led.set(0,'blue');
+		return led.set(1,'green');
+	}
+	if(led_get.blue===0 && led_get.green===1) return led.set(1,'blue');
+	if(led_get.blue===1 && led_get.green===1) return led.set(0);
+}
