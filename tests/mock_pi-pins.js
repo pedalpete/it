beef69036a861b5a1310552b167164c1197634d1
Pete Field <pedalpete@hotmail.com>
2014-07-25 21:11:17 +1000

    added onChange method

--- tests/mock_pi-pins.js
+++ tests/mock_pi-pins.js
@@ -5,14 +5,17 @@ var mock_pi_pins = {
                         },
                         value: function(val){
                             if(val){
+                                //trigger 'on so we can test it'
+                                this.on();
                                 this.pin.value=val;   
                                 return this.pin;
                             }
                             return this.pin.value || false;
                         },
+                        on: function(dir,fn){
+                        },
                         pin: {}
 };
-
 
 module.exports.connect = function(){
     return mock_pi_pins;
