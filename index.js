fe66461ed4dced8cabe385daef06dab891437aea
Pete Field <pedalpete@hotmail.com>
2014-11-09 16:55:21 +1100

    quick fix up

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
 module.exports = function(config, modules){
-        config_file = config || "./favorit.json";
+        config_file = config || "./favorit";
         return require('./lib/favor_obj_builder.js')(config_file, modules);
 }
