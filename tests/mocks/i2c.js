4778337e7e824eda2b397db7cdb9ba3b1e93e6ab
Pete Field <pedalpete@hotmail.com>
2016-02-11 19:23:13 +1100

    going back to writeByte instead of buffer

--- tests/mocks/i2c.js
+++ tests/mocks/i2c.js
@@ -13,9 +13,9 @@ var openSync = function(address) {
 	increment.call(i2c, 'open', 1);
 	return i2c;
 }
-var writeI2cBlock = function(address, addr, length, cmd, cb) {
-	increment.call(this,'writeI2cBlock',[address, addr, length, cmd.toString('utf-8')]);
-	cb.call(this, null, returnObj.call(this,[addr, cmd.toString('utf-8')]));
+var writeByte = function(address, addr, cmd, cb) {
+	increment.call(this,'writeByte',[address, addr, cmd]);
+	cb.call(this, null, returnObj.call(this,[addr, cmd]));
 };
 
 var readByte = function(address, addr, cb) {
@@ -49,12 +49,12 @@ var reset = function() {
 var mockObj = function(address) {
 	return {
 		address: address,
-		writeI2cBlock: writeI2cBlock,
+		writeByte: writeByte,
 		readByte: readByte,
 		on: on,
 		stream: stream,
 		counts: {
-			writeI2cBlock: [],
+			writeByte: [],
 			readByte: [],
 			open: []
 		},
