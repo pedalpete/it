54e1042fd19076eac4ca71a2ab7be662eaa08ba9
Pete Field <pedalpete@hotmail.com>
2016-03-25 16:15:56 +1100

    fixed query by attribute

--- tests/device_abilities.spec.js
+++ tests/device_abilities.spec.js
@@ -32,6 +32,11 @@ describe('components', function() {
 		var noMatch = $$('led.none');
 		expect(noMatch._componentMatches.length).toBe(0);
 	});
+	
+	it('should search components by attribute', function() {
+		var spiLed = $$('led[interface=spi]');
+		expect(spiLed._componentMatches.length).toBe(1);
+	});
 });
 
 describe('linked components', function() {
